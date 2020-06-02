2e34af29edc78abf6e874fba083de0e2a94f3870
Alyssa Hooper <nesteralyssa@gmail.com>
2020-06-02 11:17:33 -0700

    Adding Components

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Greeting from './Greeting';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Greeting />
   </React.StrictMode>,
   document.getElementById('root')
 );
