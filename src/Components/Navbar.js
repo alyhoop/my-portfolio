16a2e08e33b562d4600b1f57b9ef51c38872c8b3
Alyssa Hooper <nesteralyssa@gmail.com>
2020-06-04 15:26:25 -0700

    Adds NavBar Styling

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import '../CSS/index.css';
+import '../CSS/Navbar.css';
 
-function NavigationBar() {
-return(
-    <Navbar bg="dark" variant="dark" sticky="top" fixed="top">
-     <Navbar href="#home">AlyHoop</Navbar>
-     <Nav className="mr-auto">
-       <Nav.Link href="#home">Home</Nav.Link>
-       <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-       <Nav.Link href="#contact">Contact</Nav.Link>
-     </Nav>
-    </Navbar>
-  );
-
+export default class NavigationBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar bg="dark" variant="dark" sticky="top" fixed="top">
+         <Navbar href="#home">AlyHoop</Navbar>
+         <Nav className="mr-auto">
+           <Nav.Link href="#home">Home</Nav.Link>
+           <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+           <Nav.Link href="#contact">Contact</Nav.Link>
+         </Nav>
+        </Navbar>
+      </div>
+    );
+  }
 }
-
-export default NavigationBar;
