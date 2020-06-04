96c8e0a30bd1aad1291ff459bda71d6f49623af4
Alyssa Hooper <nesteralyssa@gmail.com>
2020-06-04 16:15:37 -0700

    Removal of Logo

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,7 +7,6 @@ class NavigationBar extends Component {
     return (
       <div>
         <Navbar bg="dark" variant="dark" sticky="top" fixed="top">
-         <Navbar id="name" href="#home">AlyHoop</Navbar>
          <Nav className="mr-auto">
            <Nav.Link href="#home">Home</Nav.Link>
            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
