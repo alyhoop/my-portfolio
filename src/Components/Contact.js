3db49f6d94e8243fb114e44a913384cb9bef5208
Alyssa Hooper <nesteralyssa@gmail.com>
2020-06-10 11:57:11 -0700

    Font Awesome Icon Color on Hover Change

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
