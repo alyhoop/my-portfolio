8fe43fc1ebb1fe8345fb106cd1de312b2e5a849f
Alyssa Hooper <nesteralyssa@gmail.com>
2020-06-12 16:47:38 -0700

    Portfolio Card Render

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
-import '../SCSS/Portfolio.scss'
+import { Card, Button } from 'react-bootstrap';
+import '../SCSS/Portfolio.scss';
 
 class Portfolio extends Component {
   render(){
     return(
-      <div className="container-fluid portfolio justify-content-center row">
-        <div className="portfolio-info">
-          <p>I have experience in JavaScript, React, Angular, HTML5/CSS3, SASS, Bootstrap & Git.</p>
-        </div>
-      </div>
+      //<div className="container-fluid portfolio justify-content-center row">
+      //   <div className="portfolio-info">
+      //     <p>I have experience in JavaScript, React, Angular, HTML5/CSS3, SASS, Bootstrap & Git.</p>
+      //   </div>
+      // </div>
+      <Card style={{ width: '18rem', margin: '30%'}}>
+				<Card.Img variant="top" src="https://picsum.photos/100/100" />
+				<Card.Body>
+					<Card.Title>Card Title</Card.Title>
+					<Card.Text>
+						I have experience in JavaScript, React, Angular, HTML5/CSS3, SASS, Bootstrap & Git.
+    				</Card.Text>
+					<Button variant="primary">Go somewhere</Button>
+				</Card.Body>
+			</Card>
     );
   }
 }
