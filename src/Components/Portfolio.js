01d77b2d4ba81eaf7a9fec1905aad84e9d00e7fc
Alyssa Hooper <nesteralyssa@gmail.com>
2020-06-12 16:28:43 -0700

    Centering Portfolio

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component} from 'react'
 import '../SCSS/Portfolio.scss'
 
 class Portfolio extends Component {
   render(){
     return(
-      <div className="container-fluid portfolio row">
+      <div className="container-fluid portfolio justify-content-center row">
         <div className="portfolio-info">
           <p>I have experience in JavaScript, React, Angular, HTML5/CSS3, SASS, Bootstrap & Git.</p>
         </div>
