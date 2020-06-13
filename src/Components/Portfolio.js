b513055e47c84b501bcfb3320ad2fbcf1cfd4039
Alyssa Hooper <nesteralyssa@gmail.com>
2020-06-13 13:07:38 -0700

    Portfolio CardDeck SASS

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -5,56 +5,50 @@ import '../SCSS/Portfolio.scss';
 class Portfolio extends Component {
   render(){
     return(
-      //<div className="container-fluid portfolio justify-content-center row">
-      //   <div className="portfolio-info">
-      //     <p>I have experience in JavaScript, React, Angular, HTML5/CSS3, SASS, Bootstrap & Git.</p>
-      //   </div>
-      // </div>
-
-<div style={{margin: 30, width: 900}}>
-<CardDeck>
-  <Card>
-    <Card.Img variant="top" src="https://picsum.photos/100/100" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="https://picsum.photos/100/100" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="https://picsum.photos/100/100" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-</CardDeck>
-</div>
+      <div className="portfolio">
+        <CardDeck>
+        <Card>
+          <Card.Img variant="top" src="https://picsum.photos/100/100" />
+          <Card.Body>
+            <Card.Title>Project</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="https://picsum.photos/100/100" />
+          <Card.Body>
+            <Card.Title>Project</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to additional
+              content.{' '}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="https://picsum.photos/100/100" />
+          <Card.Body>
+            <Card.Title>Project</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural lead-in to
+              additional content. This card has even longer content than the first to
+              show that equal height action.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        </CardDeck>
+      </div>
     );
   }
 }
