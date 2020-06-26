95572b32d41c06dcd1c3248d37ae368faaeac5fb
Alyssa Hooper <nesteralyssa@gmail.com>
2020-06-26 14:22:21 -0700

    Portfolio re-styling

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -10,34 +10,47 @@ class Portfolio extends Component {
         <Card style={{flex:1, backgroundColor:'#495057ff'}}>
           <Card.Img variant="top" src="./tile.jpg" />
           <Card.Body>
-            <Card.Title>Project</Card.Title>
+            <Card.Title>Portfolio</Card.Title>
             <Card.Text>
-              My role: qwe <br/>
-              Problem: qwe <br/>
-              Solution: qwe <br/>
-              <Button
-                style={{backgroundColor:'#343a40ff'}}
-                variant="secondary"
-                className="button">
-                Project
-              </Button>
-              <Button
-                style={{backgroundColor:'#343a40ff'}}
-                variant="secondary"
-                className="button">
-                Code
-              </Button>
+              <p>
+                My role: Creator
+              </p>
+              <p>
+                Problem: Wanting a responsive, modern SPA to display my progress.
+              </p>
+              <p>
+                Solution: Researched top-rated designs on Dribbble and
+                implemented a minimalist inspired page to demonstrate competency
+                using responsive design techniques.
+              </p>
+              <div className="button flex-container">
+                <Button
+                  style={{backgroundColor:'#343a40ff'}}
+                  variant="secondary"
+                >
+                  Code
+                </Button>
+              </div>
             </Card.Text>
           </Card.Body>
         </Card>
         <Card style={{flex:1, backgroundColor:'#495057ff'}}>
           <Card.Img variant="top" src="./tile.jpg" />
           <Card.Body>
-            <Card.Title>Project</Card.Title>
+            <Card.Title>Business Page Reformat</Card.Title>
             <Card.Text>
-            My role: qwe <br/>
-            Problem: qwe <br/>
-            Solution: qwe <br/>
+            <p>
+              My role: Contributor
+            </p>
+            <p>
+              Problem: About me section needed to be reformatted.
+            </p>
+            <p>
+              Solution: Bootstrap was used to dynamically re-style this page. Hard
+              coding values was avoided to accomodate varying viewports and this 
+              library was already in use on this website.
+            </p>
+            <div className="button flex-container">
               <Button
                 style={{backgroundColor:'#343a40ff'}}
                 variant="secondary"
@@ -50,29 +63,36 @@ class Portfolio extends Component {
                 className="button">
                 Code
               </Button>
+            </div>
             </Card.Text>
           </Card.Body>
         </Card>
         <Card style={{flex:1, backgroundColor:'#495057ff'}}>
           <Card.Img variant="top" src="./tile.jpg" />
           <Card.Body>
-            <Card.Title>Project</Card.Title>
+            <Card.Title>Vue Boxes</Card.Title>
             <Card.Text>
-            My role: qwe <br/>
-            Problem: qwe <br/>
-            Solution: qwe <br/>
+            <p>
+              My role: Contributor
+            </p>
+            <p>
+              Problem: An API will be sending varying lengths of information
+              inside boxes, so they needed to be styled reponsively to accomodate
+              those changes.
+            </p>
+            <p>
+              Solution: Vue's built-in breakpoints were utilized to achieve
+              consistency within this project, rather than importing a library
+              such as BootstrapVue that might slow loading time.
+            </p>
+            <div className="button flex-container">
               <Button
                 style={{backgroundColor:'#343a40ff'}}
                 variant="secondary"
                 className="button">
                 Project
               </Button>
-              <Button
-                style={{backgroundColor:'#343a40ff'}}
-                variant="secondary"
-                className="button">
-                Code
-              </Button>
+            </div>
             </Card.Text>
           </Card.Body>
         </Card>
