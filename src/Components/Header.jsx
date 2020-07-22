23ca832c646aae44c8cb30d6f93a30b9df68ddc9
Alyssa Hooper <nesteralyssa@gmail.com>
2020-07-22 16:25:26 -0700

    Updates lag on render

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
-import { Transition } from 'react-transition-group';
+import '../SCSS/index.scss';
 import '../SCSS/Header.scss';
 
 
 class Header extends Component {
      render(){
        return (
-        /* <Transition in={this.state.in} timeout={150}>
-  {state => (
-    <MyComponent className={`fade fade-${state}`} />
-  )}
-</Transition>*/
-         <div
-           className="landing flex row justify-content-center"
-           >
+         <div className="landing flex row justify-content-center" >
            <div className="title row">
              <p>
                Alyssa Hooper
