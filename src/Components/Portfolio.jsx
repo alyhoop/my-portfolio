92e6054cbfec71aa7d682fa34ef603c841712a3b
Alyssa Hooper <nesteralyssa@gmail.com>
2020-09-10 13:23:09 -0700

    Adds contact footer to project section

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Card, Button, CardDeck } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faFile } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 class Portfolio extends Component {
   render(){
@@ -154,6 +157,35 @@ class Portfolio extends Component {
           </Card.Body>
         </Card>
         </CardDeck>
+        <span className="fixed-bottom container-fluid contact p-3 my-3">
+          <span className="contact-github">
+            <a href="https://github.com/alyhoop">
+              <FontAwesomeIcon
+                icon={faGithub}
+                size="2x"
+                id="github"
+              />
+            </a>
+          </span>
+          <span className="contact-linkedin">
+            <a href="https://www.linkedin.com/in/alyssa-hooper-300b6818a/">
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                size="2x"
+                id="linkedin"
+              />
+            </a>
+          </span>
+          <span className="contact-resume">
+            <a href="./ResumeAHooper.pdf">
+              <FontAwesomeIcon
+                icon={faFile}
+                size="2x"
+                id="resume"
+              />
+            </a>
+          </span>
+        </span>
       </div>
     );
   }
