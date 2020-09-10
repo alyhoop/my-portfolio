0bcca6fb53c0f2a031e27467e3b17ab0de936122
Alyssa Hooper <nesteralyssa@gmail.com>
2020-09-10 13:21:30 -0700

    Adds contact form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,52 +1,90 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faFile } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-class Contact extends Component {
-  render(){
-    return(
-      <div className="container-fluid contact p-3 my-3 fixed-bottom">
-        <span className="contact-github">
-          <a href="https://github.com/alyhoop">
-            <FontAwesomeIcon
-              icon={faGithub}
-              size="2x"
-              id="github"
-            />
-          </a>
-        </span>
-        <span className="contact-linkedin">
-          <a href="https://www.linkedin.com/in/alyssa-hooper-300b6818a/">
-            <FontAwesomeIcon
-              icon={faLinkedin}
-              size="2x"
-              id="linkedin"
-            />
-          </a>
-        </span>
-        <span className="contact-email">
-          <a href="mailto:alyssavladahoop@gmail.com">
-            <FontAwesomeIcon
-              icon={faEnvelope}
-              size="2x"
-              id="email"
-            />
-          </a>
-        </span>
-        <span className="contact-resume">
-          <a href="./ResumeAHooper.pdf">
-            <FontAwesomeIcon
-              icon={faFile}
-              size="2x"
-              id="resume"
-            />
-          </a>
+
+export default class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      status: ""
+    };
+  }
+
+  render() {
+    const { status } = this.state;
+    return (
+      <div className="container-fluid contact p-3 my-3">
+        <FontAwesomeIcon
+          icon={faEnvelope}
+          size="2x"
+          id="email"
+        />
+        <form
+          onSubmit={this.submitForm}
+          action="https://formspree.io/meqrydly"
+          method="POST"
+        >
+          <label>Your Email:</label>
+          <input type="email" name="email" />
+          <label>Subject:</label>
+          <input type="text" name="subject" />
+          <label>Your Message:</label>
+          <input type="text" name="message" />
+          {status === "SUCCESS" ? <p>Thanks!</p> : <button>Submit</button>}
+          {status === "ERROR" && <p>Ooops! There was an error.</p>}
+        </form>
+        <span className="fixed-bottom container-fluid contact p-3 my-3">
+          <span className="contact-github">
+            <a href="https://github.com/alyhoop">
+              <FontAwesomeIcon
+                icon={faGithub}
+                size="2x"
+                id="github"
+              />
+            </a>
+          </span>
+          <span className="contact-linkedin">
+            <a href="https://www.linkedin.com/in/alyssa-hooper-300b6818a/">
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                size="2x"
+                id="linkedin"
+              />
+            </a>
+          </span>
+          <span className="contact-resume">
+            <a href="./ResumeAHooper.pdf">
+              <FontAwesomeIcon
+                icon={faFile}
+                size="2x"
+                id="resume"
+              />
+            </a>
+          </span>
         </span>
       </div>
     );
   }
+
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
 }
-
-
-export default Contact;
