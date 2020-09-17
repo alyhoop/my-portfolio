0ff1ed80fe7a57a006945054276a030342c87937
Alyssa Hooper <nesteralyssa@gmail.com>
2020-09-17 09:06:07 -0700

    Removes console error

--- src/index.js
+++ src/index.js
@@ -7,11 +7,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
